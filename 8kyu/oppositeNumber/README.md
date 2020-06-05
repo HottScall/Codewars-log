feeed9c04f204680cee3bca5a7d4581706b2439f
Scott <Scotthall1983@hotmail.com>
2020-06-05 14:09:22 +0100

    Added test cases and my process

--- 8kyu/oppositeNumber/README.md
+++ 8kyu/oppositeNumber/README.md
@@ -2,8 +2,13 @@
 
 Very simple, given a number, find its opposite.
 
-Examples:
+<h1 align="centre">Examples</h1>
 
 1: -1
 14: -14
 -34: 34
+
+<h1 align="centre">My process</h1>
+
+- Take the number argument
+- Return that argument with a minus (-)
--- 8kyu/oppositeNumber/oppositeNumber.js
+++ 8kyu/oppositeNumber/oppositeNumber.js
@@ -10,3 +10,12 @@ function opposite(number) {
 function opposite(number) {
   return -number;
 }
+
+// Test cases
+
+Test.assertEquals(opposite(1), -1);
+Test.assertEquals(opposite(0), 0);
+Test.assertEquals(opposite(4.25), -4.25);
+Test.assertEquals(opposite(3.3333333), -3.3333333);
+Test.assertEquals(opposite(-12525220.3325), 12525220.3325);
+Test.assertEquals(opposite(-5), 5);
