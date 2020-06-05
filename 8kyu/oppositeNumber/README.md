ec308c24b6d8466b777ec609e43ab3e3fa3063d6
Scott <Scotthall1983@hotmail.com>
2020-06-05 14:07:30 +0100

    Added tasks and my solution along with the best solution.

---
+++ 8kyu/oppositeNumber/README.md
@@ -0,0 +1,9 @@
+<h1 align="centre">Task</h1>
+
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
---
+++ 8kyu/oppositeNumber/oppositeNumber.js
@@ -0,0 +1,12 @@
+// My solution
+
+function opposite(number) {
+  var number2 = -number;
+  return number2;
+}
+
+// Best solution
+
+function opposite(number) {
+  return -number;
+}
