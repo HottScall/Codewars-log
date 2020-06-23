bb9f0db2bcc7f5201d6c668bf917e517501c94ac
Scott <Scotthall1983@hotmail.com>
2020-06-23 11:25:30 +0100

    Added the best solution to the js file.

--- 8kyu/bartenderDrinks/bartenderDrinks.js
+++ 8kyu/bartenderDrinks/bartenderDrinks.js
@@ -29,6 +29,32 @@ function getDrinkByProfession(param) {
   }
 }
 
+// Best Solution
+/* Turn param into lower case
+Use a switch statement which takes a case and return the word. MUCH EASIER!!
+*/
+
+function getDrinkByProfession(param) {
+  param = param.toLowerCase();
+
+  switch (param) {
+    case "jabroni":
+      return "Patron Tequila";
+    case "school counselor":
+      return "Anything with Alcohol";
+    case "programmer":
+      return "Hipster Craft Beer";
+    case "bike gang member":
+      return "Moonshine";
+    case "politician":
+      return "Your tax dollars";
+    case "rapper":
+      return "Cristal";
+    default:
+      return "Beer";
+  }
+}
+
 // Test Cases
 
 Test.assertEquals(
