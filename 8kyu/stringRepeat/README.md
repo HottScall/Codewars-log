3b0bc07646c78ebc5bc3d0ed75dc455213f12576
Scott <Scotthall1983@hotmail.com>
2020-06-27 13:25:31 +0100

    Added the String Repeat kata which I completed today.:

---
+++ 8kyu/stringRepeat/README.md
@@ -0,0 +1,11 @@
+<h1 align="centre">Task</h1>
+
+Write a function called repeatString which repeats the given String src exactly count times.
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+<h1 align="centre">My Process</h1>
+
+- Take the argument n (number)
+- Use Javascript .repeat method to chain it to the string argument (s) and give the repeat method an argument of (n) which should repeat the sting by the number within the Tests
---
+++ 8kyu/stringRepeat/stringRepeat.js
@@ -0,0 +1,37 @@
+// My soultion
+
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
+
+// Best Solution
+
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
+
+// Test CASES
+
+const solution = (n, s) => Array.from({ length: n + 1 }).join(s);
+
+describe("Tests", function() {
+  it("Basic tests", function() {
+    Test.assertSimilar(repeatStr(3, "*"), "***");
+    Test.assertSimilar(repeatStr(5, "#"), "#####");
+    Test.assertSimilar(repeatStr(2, "ha "), "ha ha ");
+    Test.assertSimilar(repeatStr(5, ">"), ">>>>>");
+    Test.assertSimilar(repeatStr(10, "!"), "!!!!!!!!!!");
+    Test.assertSimilar(repeatStr(3, "hello "), "hello hello hello ");
+    Test.assertSimilar(repeatStr(3, "$"), "$$$");
+    Test.assertSimilar(repeatStr(5, "a"), "aaaaa");
+    Test.assertSimilar(repeatStr(6, "A"), "AAAAAA");
+    Test.assertSimilar(repeatStr(7, "aA"), "aAaAaAaAaAaAaA");
+  });
+  it("Random tests", function() {
+    for (let i = 0; i < 10; i++) {
+      let num = Test.randomNumber();
+      let s = Test.randomToken()[0];
+      Test.assertSimilar(repeatStr(num, s), solution(num, s));
+    }
+  });
+});
